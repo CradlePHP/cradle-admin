1eb8cc67d2cd249c5a74bc32f181691cbf9ca507
Christian Blanquera <cblanquera@openovate.com>
2018-12-25 12:41:12 +0800

    initial commit

---
+++ src/events.php
@@ -0,0 +1,41 @@
+<?php //-->
+/**
+ * This file is part of a Custom Project.
+ * (c) 2017-2019 Acme Inc.
+ *
+ * Copyright and license information can be found at LICENSE.txt
+ * distributed with this package.
+ */
+
+/**
+ * Render admin page
+ *
+ * @param Request $request
+ * @param Response $response
+ */
+$this->on('admin-render-page', function ($request, $response) {
+    //path
+    $path = $request->getPath('string');
+    if (strpos($path, '?') !== false) {
+        $path = substr($path, 0, strpos($path, '?'));
+    }
+
+    $response->addMeta('path', $path);
+
+    $content = $this->package('cradlephp/cradle-admin')->template(
+        '_page',
+        [
+            'page' => $response->getPage(),
+            'results' => $response->getResults(),
+            'content' => $response->getContent(),
+            'i18n' => $request->getSession('i18n')
+        ],
+        [
+            'head',
+            'foot',
+            'side'
+        ]
+    );
+
+    $response->setContent($content);
+});
